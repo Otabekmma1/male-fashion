c95ca6df0d788b3044b5ba6533244baff08d29f8
Otabek Qosimov <otabek.qosimov0207@gmail.com>
2024-08-28 16:56:24 +0300

    Update settings.py

--- male_fashion/settings.py
+++ male_fashion/settings.py
@@ -142,3 +142,7 @@ TELEGRAM_CHAT_ID = 5541564692
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
