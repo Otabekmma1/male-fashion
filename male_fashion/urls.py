840aaf9c1815c82c666a8b523ef7ab64f9ad025a
Otabek Qosimov <otabek.qosimov0207@gmail.com>
2024-08-28 17:12:21 +0300

    static update

--- male_fashion/urls.py
+++ male_fashion/urls.py
@@ -23,7 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
